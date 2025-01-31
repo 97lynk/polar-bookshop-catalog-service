59bd3af14cc3f0b0f2eee2c696ae021d1eb76ed9
Tuan Nguyen <tuananh@gearment.com>
2025-01-31 16:40:36 +0700

    chapter 6: - add build image container and push to GitHub Container Registry into GitHub Actions

--- buildimage.sh
+++ buildimage.sh
@@ -1,1 +1,9 @@
 ./gradlew bootBuildImage --builder ghcr.io/thomasvitale/java -builder-arm64
+
+
+./gradlew bootBuildImage \
+  --imageName ghcr.io/<your_github_username>/catalog-service \
+  --publishImage \
+  -PregistryUrl=ghcr.io \
+  -PregistryUsername=<your_github_username> \
+  -PregistryToken=<your_github_token>
