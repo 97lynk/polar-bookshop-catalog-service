5dd1eda7601172e1597012b36fd956c53ea7c02b
Tuan Nguyen <tuananh@gearment.com>
2025-03-01 16:42:37 +0700

    Chapter 14: - use config map

--- buildimage.sh
+++ buildimage.sh
@@ -1,6 +1,6 @@
 ./gradlew bootBuildImage --builder ghcr.io/thomasvitale/java -builder-arm64
 
-
+eval $(minikube -p polar docker-env) &&
 ./gradlew bootBuildImage \
   --imageName ghcr.io/<your_github_username>/catalog-service \
   --publishImage \
@@ -9,7 +9,6 @@
   -PregistryToken=<your_github_token>
 
 
-docker image save -o image.tar  catalog-service:latest
-
+docker image save -o image.tar  catalog-service
 minikube image load image.tar --profile polar
 
